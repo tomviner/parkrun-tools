00cf21bba155200f71af3989c2df5b855e08cc05
Tom Viner <tom@viner.tv>
2019-05-11 19:15:32 +0100

    working demo

--- demo.py
+++ demo.py
@@ -3,8 +3,7 @@ import stackprinter
 
 stackprinter.set_excepthook()
 
-infile = r'hh-2019-05-11.txt'
-results = parkrun.importResults(infile,report=True)
+infile = r'hh-2019-05-11-tabs.txt'
+results = parkrun.importResults(infile, skiprows=12, skipfooter=37, report=True)
 
 parkrun.print_stats(results)
-
